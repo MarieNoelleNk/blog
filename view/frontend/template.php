f685fe9ee851356e88022c1c3703885e2fd00abc
kinaya237 <kinaya237@gmail.com>
2019-06-20 12:35:56 +0200

    deplacement vers le dossier frontend

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,29 +7,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> <?= $title ?> </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/style.css">
 </head>
 
 <body>
 
 <header>
 
-    <h1> Titre du roman </h1>
     <nav>
-        <ul>
-            <li><a href="view/login.php">Connexion</a></li>
-            <li><a href="">Accueil</a></li>
-        </ul>
+        <form class = "form-control" id="Connection" action="index.php?action=login" method="post">
+            <P id="connect_label"> Connexion </P>
+            <p><input type="text" name="login" value="xyz@example.fr" "></p>
+            <p><input type="password" name="password" value="mot de passe" "></p>
+            <p><input type="submit" name="submit" ></p>
+
+        </form>
     </nav>
 
 </header>
 
-
-
 <?= $content ?>
 
-
-<footer>
+<footer class="text-center">
 
     <p>Roman écrit en 2019</p>
 
