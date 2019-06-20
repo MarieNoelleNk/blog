5b3cdcd9d284722e428d3f1cd4d175646771a22a
kinaya237 <kinaya237@gmail.com>
2019-06-20 10:57:33 +0200

    Ajout des autres méthodes du CRUD

--- model/PostModel.php
+++ model/PostModel.php
@@ -25,5 +25,55 @@ class PostModel extends Model {
         return $post;
     }
 
+    public function addPost ($title, $content) {
+
+        $database = $this->dbconnect();
+
+        $request = $database->prepare('INSERT INTO posts (title, content, publication) VALUES (:title,:content,NOW())');
+
+        $request->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+        $request->bindValue(':content', $_POST['content'], PDO::PARAM_STR);
+
+        $request->execute(array($title, $content));
+
+        $post = $request->fetch();
+
+        return $post;
+    }
+
+    public function modifyPost ($postId, $title, $content) {
+
+        $database = $this->dbconnect();
+
+        $request = $database->prepare('UPDATE posts SET title=:title, content=:content WHERE id=:id');
+
+        $request->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+        $request->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+        $request->bindValue(':content', $_POST['content'], PDO::PARAM_STR);
+
+        $request->execute(array($postId,$title,$content));
+
+        $post = $request->fetch();
+
+        return $post;
+    }
+
+    public function removePost ($postId) {
+
+        $database = $this->dbconnect();
+
+        $comment = $database->prepare('DELETE FROM comments WHERE post_id=?');
+
+        $comment->execute([$postId]);
+
+        $request = $database->prepare('DELETE FROM posts WHERE id=? LIMIT 1');
+
+        $request->execute(array($postId));
+
+        $delete = $request->rowCount();
+
+        return $delete;
+    }
+
 }
 
