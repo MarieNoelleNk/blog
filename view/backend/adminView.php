446baf28acbb08319d91fa298820823ba620d870
kinaya237 <kinaya237@gmail.com>
2019-06-28 23:52:17 +0200

    creation d'un tableau responsive

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -3,10 +3,10 @@
 
 <?php  ob_start();?>
 
-<div class="jumbotron">
+<div class="responsive-table-line jumbotron">
     <h1 class="text-center"> Gestion des posts </h1>
 
-    <table class="table table-striped table-bordered">
+    <table class="table table-condensed table-bordered table-body-center">
 
         <thead>
         <tr>
@@ -26,15 +26,15 @@
 
             <tr>
 
-                <td><?= $post['chapter']; ?> </td>
-                <td><?= $post['title']; ?></td>
-                <td><?= $post['date_creation']; ?></td>
-                <td>
+                <td data-title="chapitre"><?= $post['chapter']; ?> </td>
+                <td data-title="titre"><?= $post['title']; ?></td>
+                <td data-title="date"><?= $post['date_creation']; ?></td>
+                <td data-title="contenu">
                     <?= mb_substr($post['content'],0,20); ?>...
                 </td>
-                <td><a href="index.php?action=editPost&amp;id=<?= $post['id']; ?>">Editer</a></td>
-                <td><a href="index.php?action=deletePost&amp;id=<?= $post['id']; ?>">Supprimer</a></td>
-                <td><a href="index.php?action=readPost&amp;id=<?= $post['id']; ?>">lire</a></td>
+                <td data-title="edition"><a href="index.php?action=editPost&amp;id=<?= $post['id']; ?>">Editer</a></td>
+                <td data-title="suppression"><a href="index.php?action=deletePost&amp;id=<?= $post['id']; ?>">Supprimer</a></td>
+                <td data-title="lecture"><a href="index.php?action=readPost&amp;id=<?= $post['id']; ?>">lire</a></td>
 
             </tr>
 
@@ -49,6 +49,7 @@
     <a href="index.php?action=getPage"><button>Ajouter un chapitre</button></a>
 
 </div>
+
 
 <?php $content= ob_get_clean();?>
 
