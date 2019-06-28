c9e4c2564bbbdc038c0ea17623796aa460952376
kinaya237 <kinaya237@gmail.com>
2019-06-28 14:19:28 +0200

    redefinition du template da l'admin

--- view/backend/adminTemplate.php
+++ view/backend/adminTemplate.php
@@ -1,39 +1,70 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <script src="https://cdn.tiny.cloud/1/lxs97m54mcfenmx34hi42djka9fj14gzia10cwic7wi9slt5/tinymce/5/tinymce.min.js"></script>
+    <script>tinymce.init({selector:'textarea'});</script>
+    <link href="public/assets/owl.carousel.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/style1.css">
 
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> <?= $title ?></title>
-        <script src="https://cdn.tiny.cloud/1/lxs97m54mcfenmx34hi42djka9fj14gzia10cwic7wi9slt5/tinymce/5/tinymce.min.js"></script>
-        <script>tinymce.init({selector:'textarea'});</script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="public/style.css">
-    </head>
+<body>
 
-    <body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">Jean FtR</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarReponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link active login-btn" href="index.php?action=adminPost">Chapitres</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active login-btn" href="index.php?action=showComment">Commentaires</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active login-btn" href="index.php?action=disconnect">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-    <header>
+<section id="admin_background">
+    <?= $content ?>
+</section>
+<footer class="container-fluid bg-dark">
 
-        <a href="index.php?action=disconnect">Deconnexion</a>
+    <div class="row text-center padding">
 
-    </header>
+        <div class="col-md-6">
+            <h2>Me suivre</h2>
+        </div>
+        <div class="col-md-6 social padding">
+            <ul class="social-icon animate">
+                <li><a href="#" title="facebook" target="_blank"><i class="fab fa-facebook"></i></a></li> <!-- change the link to social page and edit title-->
+                <li><a href="#" title="twitter" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#" title="google plus" target="_blank"><i class="fab fa-google-plus"></i></a></li>
+            </ul>
+        </div>
 
-    <section>
+    </div>
 
-        <?= $content ?>
+</footer>
 
-    </section>
-
-    <footer class="text-center">
-
-        <p>Roman écrit en 2019</p>
-
-    </footer>
-
-    </body>
+</body>
 
 </html>
+
 
